26da23af0e5d9cf18d6c3e44677101c83d1ce030
Jose Navas <josenavasmolina@gmail.com>
2016-06-30 08:00:28 -0700

    Adding coverage, travis, fixing the setup and renaming folder'

--- setup.py
+++ setup.py
@@ -30,22 +30,20 @@ with open('README.rst') as f:
 
 classifiers = [s.strip() for s in classes.split('\n') if s]
 
-setup(name='qiita-type-plugin-target-gene',
+setup(name='qtp-target-gene',
       version=__version__,
       long_description=long_description,
       license="BSD",
       description='Qiita Type Plugin: Target Gene',
       author="Qiita development team",
       author_email="qiita.help@gmail.com",
-      url='https://github.com/biocore/qiita',
+      url='https://github.com/qiita-spots/qtp-target-gene',
       test_suite='nose.collector',
-      packages=['target_gene_type'],
-      package_data={
-        'target_gene_type': [
-            'support_files/config_file.cfg',
-            'tests/test_data/*']},
+      packages=['qtp_target_gene'],
+      package_data={'qtp_target_gene': ['support_files/config_file.cfg',
+                                        'tests/test_data/*']},
       scripts=glob('scripts/*'),
-      extras_require={'test': ["nose >= 0.10.1", "pep8", 'httpretty']},
+      extras_require={'test': ["nose >= 0.10.1", "pep8"]},
       install_requires=['click >= 3.3', 'h5py >= 2.3.1'],
       classifiers=classifiers
       )
