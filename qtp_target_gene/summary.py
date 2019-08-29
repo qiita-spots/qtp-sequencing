f157f32577f9e2dc78fea457bc93c9de1bb4d303
Antonio Gonzalez <antgonza@gmail.com>
2019-08-29 13:47:05 -0600

    # noqa

--- qtp_target_gene/summary.py
+++ qtp_target_gene/summary.py
@@ -16,8 +16,9 @@ from io import StringIO
 from qiita_files.demux import stats as demux_stats
 import matplotlib
 
-
 matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt # noqa
 
 FILEPATH_TYPE_TO_NOT_SHOW_HEAD = ['SFF']
 LINES_TO_READ_FOR_HEAD = 10
@@ -181,12 +182,12 @@ def _summary_demultiplexed(artifact_type, filepaths):
     artifact_information.append("<br/>")
 
     # taken from http://stackoverflow.com/a/9141911
-    matplotlib.pyplot.bar(shist_edge[:-1], shist, width=1)
-    matplotlib.pyplot.xlim(min(shist_edge), max(shist_edge))
-    matplotlib.pyplot.xlabel('Sequence Length')
-    matplotlib.pyplot.ylabel('Number of sequences')
+    plt.bar(shist_edge[:-1], shist, width=1)
+    plt.xlim(min(shist_edge), max(shist_edge))
+    plt.xlabel('Sequence Length')
+    plt.ylabel('Number of sequences')
     plot = StringIO()
-    matplotlib.pyplot.savefig(plot, format='png')
+    plt.savefig(plot, format='png')
     plot.seek(0)
     uri = 'data:image/png;base64,' + quote(b64encode(plot.buf))
     artifact_information.append('<img src = "%s"/>' % uri)
